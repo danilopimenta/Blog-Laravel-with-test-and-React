79e917e77cdde0a7909d806232561d47c89133ea
Danilo Pimenta Aguiar <danilopi@outlook.com>
2017-05-07 15:22:43 -0300

    get url version when invoke

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,9 +5,9 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Bloguinho</title>
 
-        <link rel="stylesheet" href="css/front.css">
+        <link rel="stylesheet" href="{{ mix('css/front.css') }}">
     </head>
     <body>
     <div class="page">
@@ -15,5 +15,5 @@
         <main id="front" class="container"></main>
     </div>
     </body>
-    <script src="js/front.js" ></script>
+    <script src="{{ mix('js/front.js') }}" ></script>
 </html>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 
     <!-- Scripts -->
     <script>
@@ -85,6 +85,6 @@
     <script src="https://code.jquery.com/jquery-2.2.4.min.js"
             integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
             crossorigin="anonymous"></script>
-    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
